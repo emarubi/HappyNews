4db7c17609ca5e6f46cba6afd7932e334da1451d
thobenayann <thobena.yann@orange.fr>
2021-01-20 13:58:48 +0100

    chore(home):header-integration

--- FRONT/src/components/Header/index.js
+++ FRONT/src/components/Header/index.js
@@ -1,13 +1,28 @@
 // import NPM
 import React from 'react';
-
-// import CSS
+//import Utilitaire, modules
+import avatarPhoto from '../../assets/Images/avatar-header.jpg';
+import logo from '../../assets/Images/Logo-Happy-News.png';
+//import CSS
 import './style.scss';
+import Title from './Title';
 
-const Header = () => (
-  <div>
-    je suis le header
-  </div>
-);
+
+const Header = () => {
+    return (
+      <header className='header'>
+        <a href='#' >
+          <img src={logo} alt="logo website" className='header-logo' />
+        </a> 
+          <Title />
+          <div className='header-avatar-picture'>
+          <img src={avatarPhoto} alt="my-avatar" className='header-avatar' />
+          <button className="header-button"> se connecter </button> 
+          <button className="header-button-news"> liste des news </button> 
+      </div>
+      </header>
+    )
+}
+
 
 export default Header;
