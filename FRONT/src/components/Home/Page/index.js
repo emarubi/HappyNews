d2b707216513cb852334672570d9c614757a6432
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-20 17:14:34 +0100

    chore(page): css implementation

--- FRONT/src/components/Home/Page/index.js
+++ FRONT/src/components/Home/Page/index.js
@@ -8,23 +8,27 @@ import './style.scss';
 function Page() {
     return (
         <div className="container">
-            <section className="map">
-                <div className="inputSearchMap">
-                        <input type="text" className="serachOnMap"/>
-                    </div>
-                <div className="mapSection">
+            <section className="mapSection">
+                <div className="contentInput">
+                    <input type="text" className="searchOnMap"/>
+                    <label className="textLabel">Rechercher</label>
                 </div>
+                <img className="mapPict" src={ Map } alt="map"/>
             </section>
             
-            <section className="custommers">
+            <section className="customers">
                 <img className="customerPict" src={ Customer } alt="customer"/>
-                <h2 className="titleCustommers">Vous êtes Clients</h2>
-                <p className="sectionLandingPageText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, laudantium id hic enim ut corrupti eum odio facilis dolore earum alias veniam? Nisi consectetur quos distinctio. Repellendus dolorum id quisquam!</p>
+                <div className="customerText">
+                    <h2 className="titleCustomers">Vous êtes Client</h2>
+                    <p className="sectionLandingPageText">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, laudantium id hic enim ut corrupti eum odio facilis dolore earum alias veniam? Nisi consectetur quos distinctio. Repellendus dolorum id quisquam!</p>
+                </div>
             </section>
 
             <section className="retailers">
-                <h2 className="titleRetailers">Vous êtes Commerçant</h2>
-                <p className="sectionLandingPageText">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita officia quia alias voluptates? Corporis magni facere sint assumenda porro ipsum! Iste laborum repellat provident qui numquam possimus totam porro? Et?</p>
+                <div className="retailerText">
+                    <h2 className="titleRetailers">Vous êtes Commerçant</h2>
+                    <p className="sectionLandingPageText">Lorem ipsum, dolor    sit amet consectetur adipisicing elit. Expedita officia    quia alias voluptates? Corporis magni facere sint  assumenda porro ipsum! Iste laborum repellat provident   qui numquam possimus totam porro? Et?</p>
+                </div>
                 <img className="retailerPict" src={ Retailer } alt="retailer"/>
             </section>
             
