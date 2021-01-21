525b30bd9d80f1400cce3e5c9d1f3124ccdc2b0c
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-21 11:16:52 +0100

    chore(page): Creation Map component

--- FRONT/src/components/Home/Page/index.js
+++ FRONT/src/components/Home/Page/index.js
@@ -9,39 +9,34 @@ function Page() {
     return (
         <body>
             {/** ------------- Map Section ---------- */}
-            <section className="mapSection">
-                <div className="wrap">
-                    <div className="content">
-
-                    </div>
-                    {/** ------------- Wave shape ---------- */}
-                    <div class="custom-shape-divider-bottom-1611212523">
-                        <svg className="wave" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-                        </svg>
-                    </div>
-                </div>
-            </section>
+           
 
             {/** ------------- Customer Section ---------- */}
             <section className="character customer">
-                <div className="pictureCharacter">
-                    <img className="illustrationPicture" src={ Customer } alt="customer"/>
+                <div className="pictureCharacter customer">
+                    <img className="illustrationPicture customer" src={ Customer } alt="customer"/>
                 </div>
-                <div className="presentationText">
-                    <h2 className="presentationTitle">Vous êtes Client</h2>
-                    <p className="presentationBody">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, laudantium id hic enim ut corrupti eum odio facilis dolore earum alias veniam? Nisi consectetur quos distinctio. Repellendus dolorum id quisquam!</p>
+                <div className="presentationText customer">
+                    <h2 className="presentationTitle customer">Vous êtes Client</h2>
+                    <p className="presentationBody customer">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, laudantium id hic enim ut corrupti eum odio facilis dolore earum alias veniam? Nisi consectetur quos distinctio. Repellendus dolorum id quisquam!</p>
                 </div>
             </section>
 
             {/** ------------- Retailer Section ---------- */}
             <section className="character retailer">
-                <div className="presentationText">
-                    <h2 className="presentationTitle">Vous êtes Commerçant</h2>
-                    <p className="presentationBody">Lorem ipsum, dolor    sit amet consectetur adipisicing elit. Expedita officia    quia alias voluptates? Corporis magni facere sint  assumenda porro ipsum! Iste laborum repellat provident   qui numquam possimus totam porro? Et?</p>
+            <div class="custom-shape-divider-top-1611223784">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+            
+                <div className="presentationText retailer">
+                
+                    <h2 className="presentationTitle retailer">Vous êtes Commerçant</h2>
+                    <p className="presentationBody retailer">Lorem ipsum, dolor    sit amet consectetur adipisicing elit. Expedita officia    quia alias voluptates? Corporis magni facere sint  assumenda porro ipsum! Iste laborum repellat provident   qui numquam possimus totam porro? Et?</p>
                 </div>
                 <div className="pictureCharacter">
-                    <img className="illustrationPicture" src={ Retailer } alt="retailer"/>
+                    <img className="illustrationPicture retailer" src={ Retailer } alt="retailer"/>
                 </div>
             </section>
             
