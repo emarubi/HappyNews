4fe26885b807cd27bd557788e179d1853904885d
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-21 11:47:41 +0100

    chore(page): Creation Retailer component

--- FRONT/src/components/Home/Page/Customer/index.js
+++ FRONT/src/components/Home/Page/Customer/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 import CustomPict from '../../../../assets/Images/customer.png';
 
-
-
-
+import './style.scss';
 
 const Customer = () => {
   return (
---
+++ FRONT/src/components/Home/Page/Retailer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import RetailPict from '../../../../assets/Images/retailer.png';
+
+import './style.scss';
+
+const Retailer = () => {
+  return (
+    <body>
+      <section className="character retailer">
+
+        <div className="presentationText retailer">
+          <h2 className="presentationTitle retailer">Vous êtes Commerçant</h2>
+          <p className="presentationBody retailer">Lorem ipsum, dolor    sit amet consectetur adipisicing elit. Expedita officia    quia alias voluptates? Corporis magni facere sint  assumenda porro ipsum! Iste laborum repellat provident   qui numquam possimus totam porro? Et?</p>
+        </div>
+
+        <div className="pictureCharacter">
+            <img className="illustrationPicture retailer" src={ RetailPict } alt="retailer"/>
+        </div>
+
+      </section> 
+    </body>
+  )
+}
+
+export default Retailer;
+
