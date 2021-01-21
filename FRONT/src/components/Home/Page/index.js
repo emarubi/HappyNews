43f77b24c7549b7b4c1cc391a87514aca5e94f65
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-21 08:14:54 +0100

    chore(page): change organization and wave on map

--- FRONT/src/components/Home/Page/index.js
+++ FRONT/src/components/Home/Page/index.js
@@ -7,8 +7,41 @@ import './style.scss';
 
 function Page() {
     return (
-        <div className="container">
+        <body>
             <section className="mapSection">
+                <div className="wrap">
+                    <div className="content">
+
+                    </div>
+                    <div class="custom-shape-divider-bottom-1611212523">
+                        <svg className="wave" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                        </svg>
+                    </div>
+                </div>
+            </section>
+            
+        </body>
+        
+          
+    )
+}
+
+export default Page
+
+{/*
+    -------------------- Clip Path Circle
+    <body>
+            <section>
+                <div className="content">
+                     <h2>Composant non rectangulaire</h2>
+                </div>
+            </section> 
+        </body> */}
+
+{/**
+    ---------------------- Paco /Home component
+    <section className="mapSection">
                 <div className="contentInput">
                     <input type="text" className="searchOnMap"/>
                     <label className="textLabel">Rechercher</label>
@@ -25,15 +58,10 @@ function Page() {
             </section>
 
             <section className="retailers">
+                
                 <div className="retailerText">
                     <h2 className="titleRetailers">Vous êtes Commerçant</h2>
                     <p className="sectionLandingPageText">Lorem ipsum, dolor    sit amet consectetur adipisicing elit. Expedita officia    quia alias voluptates? Corporis magni facere sint  assumenda porro ipsum! Iste laborum repellat provident   qui numquam possimus totam porro? Et?</p>
                 </div>
                 <img className="retailerPict" src={ Retailer } alt="retailer"/>
-            </section>
-            
-        </div>
-    )
-}
-
-export default Page
+            </section> */}
