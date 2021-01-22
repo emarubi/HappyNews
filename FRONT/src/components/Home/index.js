1cec15c78635f14657da556b7a80506a394348b0
asmatar <deruelle.arthur@gmail.com>
2021-01-22 15:06:41 +0100

    feat (front/router): road map

--- FRONT/src/components/Home/index.js
+++ FRONT/src/components/Home/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
-// import Footer from 'src/components/Footer';
-// import About from 'src/components/Footer/About';
-// import CGU from 'src/components/Footer/CGU';
-// import Terms from 'src/components/Footer/Terms';
-// import Header from 'src/components/Header';
-// import ContactForm from '../Footer/ContactForm';
 // import RegisterForm from 'src/components/Register';
-// import About from 'src/components/Footer/About';
-// import Header from 'src/components/Header';
-// import NotFound from '../404';
 import Page from 'src/components/Home/Page';
 // import Login from 'src/components/Login';
 const Home = () => (
   <div>
-    {/* <Header /> */}
+
     <Page />
     {/* pour tester le temps de faire des routes */}
     {/* <RegisterForm /> */}
-    {/* <CGU /> */}
-    {/* <Terms /> */}
-    {/* <About /> */}
-    {/* <NotFound /> */}
-    {/* <Login /> */}
-    {/* <Footer /> */}
+
   </div>
 );
 
