a743bd94bf556face95f17e1835c411548d4453a
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-22 12:34:00 +0100

    chore(custommerProfile): css profile page arrangement

--- FRONT/src/components/Home/Page/CustomerProfile/index.js
+++ FRONT/src/components/Home/Page/CustomerProfile/index.js
@@ -1,35 +1,55 @@
-import React from 'react'
+import React from 'react';
 
 import ShopCard from '../../../../assets/Images/shopCard.png';
+import ProfilePict from '../../../../assets/Images/profile_pic.png';
+
+import './style.scss'
 
 const CustomerProfile = () => {
   return (
-    <section>
+    <section className="general">
       <div className="frame">
         <div className="title-section">
           <h2 className="title">Profil Client</h2>
         </div>
 
         <div className="top-section">
-          <div className="pict-container">
-            <img src="" alt="" className="profile-pict"/>
-          </div>
-          <div className="text-container">
-          <p className="profile-description"></p>
+          <h3 className="title-section">Profil</h3>
+          <div className="content-profile">
+            <div className="left-side">
+              <div className="pict-container">
+                <img src={ ProfilePict } alt="" className="profile-pict"/>
+              </div>
+              <div className="text-container">
+                <ul className="profile-list">
+                  <li>Nom:</li>
+                  <li>Prénom:</li>
+                  <li>Age:</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="right-side">
+              <div className="content-right-side">
+                <button>Modifier le profil</button>
+              </div>
+            </div>
+
           </div>
         </div>
 
         <div className="bottom-section">
           <div className="title-bottom-section">
             <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
+              <li>Mes News</li>
+              <li>Mes Achats</li>
+              <li>Mes Favoris</li>
             </ul>
           </div>
           <div className="content-bottom-section">
             <div className="card-one">
-              première card
+              <h3 className="title-card">Titre</h3>
+              <img className="card-Pict" src={ ShopCard } alt="shop card"/>
             </div>
           </div>
         </div>
@@ -38,4 +58,4 @@ const CustomerProfile = () => {
   )
 }
 
-export default ShopCard;
+export default CustomerProfile;
--- FRONT/src/components/Home/Page/index.js
+++ FRONT/src/components/Home/Page/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Maps from './Map/index';
 import Customer from './Customer/index';
 import Retailer from './Retailer/index';
+import CustomerProfile from './CustomerProfile/index';
 
 import './style.scss';
 
 function Page() {
     return (
         <div>
-            <Maps />
+            {/** 
+             * <Maps />
             <Customer />
             <Retailer />
+            */}
+            <CustomerProfile />
+            
         </div>
     )
 }
