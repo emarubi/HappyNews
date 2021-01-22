07981d8066c37c99dcfe09dc6af7dab9eb03c9de
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-22 15:02:41 +0100

    chore(newsModal): add icons on card

--- FRONT/src/components/NewsModal/index.js
+++ FRONT/src/components/NewsModal/index.js
@@ -19,8 +19,16 @@ const NewsModal = () => {
 			  <div class="product-bottom-details">
 				  <div class="product-price"><small>$96.00</small>$230.99</div>
 				    <div class="product-links">
-					    <a href=""><i class="fa fa-heart"></i></a>
-					    <a href=""><i class="fa fa-shopping-cart"></i></a>
+					    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#3FAFAC" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  							<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  							<path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+							</svg><i class="fa fa-heart"></i></a>
+					    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#3FAFAC" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  							<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  							<circle cx="9" cy="19" r="2" />
+  							<circle cx="17" cy="19" r="2" />
+  							<path d="M3 3h2l2 12a3 3 0 0 0 3 2h7a3 3 0 0 0 3 -2l1 -7h-15.2" />
+							</svg><i class="fa fa-shopping-cart"></i></a>
 				    </div>
 			    </div>
 		    </div>
