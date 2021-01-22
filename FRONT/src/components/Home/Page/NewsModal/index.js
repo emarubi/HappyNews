d9e737484386c7e3c8f9c1b5ae897f134bc5d185
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-22 13:56:25 +0100

    chore(newsModal): files creation with css and  html structure

---
+++ FRONT/src/components/Home/Page/NewsModal/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.scss';
+
+const NewsModal = () => {
+  return (
+    <section>
+      <h2>Test</h2>
+    </section>
+  )
+}
+
+export default NewsModal;
