51015a46d32c86961e392656581ed864e59e7bac
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-22 19:39:52 +0100

    chore(AddNewsForm): htmlstructure form

--- FRONT/src/components/AddNewsForm/index.js
+++ FRONT/src/components/AddNewsForm/index.js
@@ -4,8 +4,47 @@ import './style.scss';
 
 const AddNewsForm = () => {
   return (
-    <section>
-      <h2>Test AddnewsForm</h2>
+    <section className="section-addnews-form">
+      <div className="wraper">
+        <div className="form-container">
+          <span className="form-heading">Ajouter une News</span>
+          <form action="" className="form-body">
+            <div className="news-title">
+              <i className="fas fas-title"></i>
+              <input type="text"placeholder="Titre..." required/>
+              <span className="bar"></span>
+            </div>
+            <div className="input-group">
+              <i className="news-description"></i>
+              <input type="text"placeholder="Description..." required/>
+              <span className="bar"></span>
+            </div>
+            <div className="input-group">
+              <i className="news-category"></i>
+              <input type="text"placeholder="Catégorie..." required/>
+              <span className="bar"></span>
+            </div>
+            <div className="input-group">
+              <i className="news-price"></i>
+              <input type="number"placeholder="Prix..." required/>
+              <span className="bar"></span>
+            </div>
+            <div className="input-group">
+              <i className="picture-downlaod"></i>
+              <input type="file" accept="image" required/>
+              <span className="bar"></span>
+            </div>
+            <div className="input-group">
+              <button className="news-valid-form-but">
+                <i className="picture-valid"></i>
+              </button>
+              <div className="switch-login">
+                <a href="#">Déjà un compte? <span>Login</span></a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
