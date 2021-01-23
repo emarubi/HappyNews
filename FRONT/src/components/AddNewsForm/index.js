b6d2b0d0daef1c2ab987cfc9ce5ebd1c63fbc9d7
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-23 10:48:16 +0100

    chore(AddNewsForm): add second button to delete and adjustements

--- FRONT/src/components/AddNewsForm/index.js
+++ FRONT/src/components/AddNewsForm/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Send from '../../assets/Images/send.png';
+
 import './style.scss';
 
 const AddNewsForm = () => {
@@ -36,11 +38,27 @@ const AddNewsForm = () => {
             </div>
             <div className="input-group">
               <button className="news-valid-form-but">
-                <i className="picture-valid"></i>
+                <i className="picture-valid">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-telegram" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+                  </svg>
+                </i>
               </button>
-              <div className="switch-login">
+              <button className="news-valid-form-but">
+                <i className="picture-valid">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </i>
+              </button>
+              {/**
+               * <div className="switch-login">
                 <a href="#">Déjà un compte? <span>Login</span></a>
               </div>
+               */}
             </div>
           </form>
         </div>
