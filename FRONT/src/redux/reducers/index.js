791e6cc6d6498fdc712ad46bb05a5df58285d178
thobenayann <thobena.yann@orange.fr>
2021-01-26 14:08:45 +0100

    feat(front/middleware-axios-newsList):axios-newsListReducer-dynamicDataOnNewsPage

--- FRONT/src/redux/reducers/index.js
+++ FRONT/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
-
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import reducerAddNews from './reducerAddNews';
+import newsReducer from './news';
+
 export default combineReducers({
-    auth: authReducer,
-    addNews: reducerAddNews
-  });
+  auth: authReducer,
+  addNews: reducerAddNews,
+  newsList: newsReducer,
+});
--- FRONT/src/redux/store.js
+++ FRONT/src/redux/store.js
@@ -3,14 +3,16 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { reducerAddNews } from './reducers/reducerAddNews';
 // import thunk from 'redux-thunk';
 import authMiddleware from '../middlewares/auth';
+// import de mon middleware d'appel a api (requête pour obtenir les news)
+import api from '../middlewares/api';
 // rootReducer = résultat de combineReducers
 import rootReducer from './reducers';
 
 // on crée le store
-const store = createStore (rootReducer,composeWithDevTools(applyMiddleware(
-    // commentaire de auth mid pour thunk
-    authMiddleware
-    // thunk
+const store = createStore (rootReducer, composeWithDevTools(applyMiddleware(
+  // commentaire de auth mid pour thunk
+  authMiddleware,
+  api,
 )));
 
 export default store;
