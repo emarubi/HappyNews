d603245746ee8c8f9e0150eab1a39cbeab012f1a
thobenayann <thobena.yann@orange.fr>
2021-01-27 07:36:53 +0100

    news-container:dispatch-action-get-activities

--- FRONT/src/containers/news.js
+++ FRONT/src/containers/news.js
@@ -6,12 +6,18 @@ import News from 'src/components/News';
 const mapStateToProps = (state) => ({
   // on crée un boolen qui vaut vrai si on a des news sont dans la liste
   hasData: state.newsList.list.length > 0,
+  // on crée la data qui liste nos news
   list: state.newsList.list,
+  // on crée la data qui liste nos activités (= catégories dans notre page News)
+  all_activities: state.all_activities,
 });
 // mapDispatchToProps = cablage des actions (props de type fonction)
 const mapDispatchToProps = (dispatch) => ({
   loadNews: () => {
     dispatch({ type: 'GET_NEWS' });
+  },
+  loadActivities: () => {
+    dispatch({ type: 'GET_ACTIVITIES' });
   },
 });
 
