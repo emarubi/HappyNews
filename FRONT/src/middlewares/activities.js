f5c13886e1129b2a524e7e55b143828c609ade0c
thobenayann <thobena.yann@orange.fr>
2021-01-27 07:27:40 +0100

    middleware-activities

---
+++ FRONT/src/middlewares/activities.js
@@ -0,0 +1,20 @@
+// activities = catégories dans notre page News
+import axios from 'axios';
+
+const api = (store) => (next) => (action) => {
+  switch (action.type) {
+    case 'GET_ACTIVITIES':
+      // on déclenche la requete 
+      axios.get('https://api-happy-news.herokuapp.com/activity')
+        .then((response) => { // requete réussie
+          // on envoie une action pour sauvegarder les activités
+          // avec un second paramètre qui contient la réponse
+          store.dispatch({ type: 'GET_ACTIVITIES_SUCCESS', list: response.data });
+        });
+      break;
+    default:
+      next(action);
+  }
+};
+
+export default api;
