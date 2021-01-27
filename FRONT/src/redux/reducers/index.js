b053656a022c3c11baa43144fb5719f2d9e1f120
Nicolas Plaza <pacorabo2@hotmail.com>
2021-01-27 13:05:07 +0100

    fix(reducers/index.js): rename of key

--- FRONT/src/redux/reducers/index.js
+++ FRONT/src/redux/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
-import reducerAddNews from './reducerAddNews';
+import addNews from './reducerAddNews';
 import newsReducer from './news';
 
 export default combineReducers({
   auth: authReducer,
-  addNews: reducerAddNews,
+  addNews,
   newsList: newsReducer,
 });
