f23e4c505fe95c27f7822f0bb6038e83800101fd
emarubi <emarubi@gmail.com>
2021-01-29 18:39:11 +0100

    fix(geoloc): fix axios request in map component

--- FRONT/src/components/Home/Page/Map/index.js
+++ FRONT/src/components/Home/Page/Map/index.js
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   MapContainer, TileLayer, Marker, Popup, useMapEvents,
 } from 'react-leaflet';
-
+import { Icon } from 'leaflet';
 // import users from './user';
 import './style.scss';
 
-const Maps = () => {
-  const users = [
-    {
-      id: 1, latitude: 43.7413633, longitude: 7.2471444, shop_name: 'paper plane',
-    },
-    {
-      id: 2, latitude: 43.742224, longitude: 7.239953, shop_name: 'Tout est bon dans le cochon',
-    },
-    {
-      id: 5, latitude: 43.739581, longitude: 7.237217, shop_name: "L'Art du Pain",
-    },
-  ];
+// const visitorIcon = new Icon({
+//   iconUrl: '/'
+// })
 
+const Maps = () => {
   // define a state for an activeUser, when the visitor click on a user Marker on the map
   // if the vsitor doesn't select a user Marker, the activeUser is set at null in the state
   const [activeUser, setActiveUser] = useState(null);
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api-happy-news.herokuapp.com/user')
+      .then((response) => {
+        console.log(response);
+        setUsers(response.data.data);
+      })
+      .catch((error) => {
+      });
+  }, []);
+
+  console.log('users après requête axios', users);
 
   // const position = [48.856614, 2.3522219]; // coordonnées GPS de Paris
   // visitor geoLocalisation on the Map
@@ -48,7 +54,11 @@ const Maps = () => {
 
   return (
 
-    <MapContainer className="mapContainer" center={[48.856614, 2.3522219]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[48.856614, 2.3522219]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -56,18 +66,22 @@ const Maps = () => {
 
       <LocationMarker />
 
-      {users.map((user) => (
-        <Marker
-          key={user.id}
-          position={[user.latitude, user.longitude]}
-          eventHandlers={{
-            click: () => {
-              console.log('marker clicked', user.shop_name);
-              setActiveUser(user);
-            },
-          }}
-        />
-      ))}
+      {users.length
+        && users.map((user) => (
+          (!isNaN(parseFloat(user.latitude)) || !isNaN(parseFloat(user.longitude)))
+            && (
+            <Marker
+              key={user.id}
+              position={[parseFloat(user.latitude), parseFloat(user.longitude)]}
+              eventHandlers={{
+                click: () => {
+                  console.log('marker clicked', user.shop_name);
+                  setActiveUser(user);
+                },
+              }}
+            />
+            )
+        ))}
       {/* check if there is an active user (if the visitor click on is marker), if true, it shows a Popup */}
       {activeUser && (
         <Popup
