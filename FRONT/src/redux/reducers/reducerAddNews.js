b30ba60f23a51e9cfd79805917bc62d5a58eb68e
emarubi <emarubi@gmail.com>
2021-02-01 11:05:50 +0100

    merge(front/main-integration): avant pull

---
+++ FRONT/src/redux/reducers/reducerAddNews.js
@@ -0,0 +1,32 @@
+
+import { ADD_NEWS, CHANGE_ADDNEWS_FIELD } from '../actions';
+
+// Initial State
+const initialState = {
+    article_title: '',
+    description: '',
+    picture_url: '',
+    price: '',
+    is_news: true, 
+    user_id: null, // A récupérer depuis le localstorage
+    activity_id: '', // A récupérer depuis un champs déroulant. 9 id différents à récup dans le back
+}
+// reducer
+
+const reducerAddNews = (oldState = initialState, action) => {
+  console.log('oldState dans le reducer:', oldState)
+
+  switch (action.type) {
+    
+    case CHANGE_ADDNEWS_FIELD:
+      return {
+        ...oldState,
+        [action.name]:action.value,
+      }
+      default:
+      return { ...oldState};
+  }
+}
+
+export default reducerAddNews;
+
