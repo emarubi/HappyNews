4262cd18d58bf8164145bba6fd25087be3d02dfe
asmatar <deruelle.arthur@gmail.com>
2021-02-02 01:17:41 +0100

    manage is true news for rendering

--- FRONT/src/components/RetaillerP/Tabs/index.js
+++ FRONT/src/components/RetaillerP/Tabs/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/button-has-type */
 import React, { useState } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import NewsModal from 'src/components/NewsModal';
 import './style.scss';
 import TabTitle from './TabTitle';
@@ -12,15 +11,22 @@ const Tabs = ({ user, news }) => {
   const toggleTab = (index) => {
     setToggleState(index);
   };
-
+  const { id } = useParams();
+  console.log(id);
   // const { id } = useParams();
   // console.log(news);
 
   // const newsByUserId = news.filter((article) => article.user_id = id);
   // console.log(newsByUserId);
   console.log(news);
-  const newsIsNews = news.filter((news) => (news.is_news === true));
-  console.log(newsIsNews);
+  const newsIsNewsTrue = news.filter((news) => (news.is_news === true));
+  console.log(newsIsNewsTrue);
+
+  const newsIsNewsFalse = news.filter((news) => (news.is_news === false));
+  console.log(newsIsNewsFalse);
+
+  const newsUserId = news.filter((news) => (news.user_id === id));
+  console.log(newsUserId);
 
   return (
     <div className="container">
@@ -52,7 +58,7 @@ const Tabs = ({ user, news }) => {
           <p>
             {/* // news.map((item) => <li> {item.id}</li>) */}
             {
-                news.map((news) => (
+                newsIsNewsTrue.map((news) => (
                   <div key={news.id} className="newsList__item">
                     <NewsModal news={news} />
 
@@ -64,14 +70,14 @@ const Tabs = ({ user, news }) => {
 
         <div className={toggleState === 3 ? 'content  active-content' : 'content'}>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, quos
-            qui itaque, perferendis ab ipsam, veniam dolores porro maxime earum nesciunt
-            repellendus exercitationem hic veritatis vero. Nisi magnam minus aperiam aliquam
-            nulla minima ducimus quibusdam? Sit, velit! Officiis soluta quaerat dicta, ad facere
-            necessitatibus possimus est repellat pariatur. Facere molestiae cupiditate numquam
-            aliquam fuga repellendus quo aut culpa, sint, qui quam. Quae voluptate,
-            obcaecati nesciunt deleniti amet praesentium laborum atque, ipsam
-            molestiae a quasi provident accusantium sequi, quam unde sint.
+            {
+                newsIsNewsFalse.map((news) => (
+                  <div key={news.id} className="newsList__item">
+                    <NewsModal news={news} />
+
+                  </div>
+                ))
+              }
           </p>
         </div>
 
