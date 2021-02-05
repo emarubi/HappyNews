2843611357f447f01276ea47ac5c7349bba29291
asmatar <deruelle.arthur@gmail.com>
2021-02-05 23:53:47 +0100

    last version

--- FRONT/src/components/AddNewsForm/index.js
+++ FRONT/src/components/AddNewsForm/index.js
@@ -12,7 +12,7 @@ export const AddNewsForm = ({
      const handleChangeImage = e => {
     const reader = new FileReader() // Je crée un nouveau reader
     const file = e.target.files[0] // Je récupère mon image
-    reader.onloadend =() =>{ // Je la converti en blob afin de pouvoir l'envoyer au back.
+    reader.onloadend =() =>{ // Je la converti en blob afiyarn de pouvoir l'envoyer au back.
       // console.log('reader.result', reader.result)
       handleChangeField('picture_url', reader.result)
     }
@@ -26,90 +26,88 @@ export const AddNewsForm = ({
     handleAddNews();
   };
 
-
   return (
 
-
-      <section className="section-addnews-form">
-        <div id="wraper">
-          <div className="form-container">
-            <span className="form-heading">Ajouter une News</span>
-            <form
-              method="post"
-              className="form-body"
-              onSubmit={handleSubmit}
-            >
-              <div className="input-group">
-                <i className="news-title" />
-                <input
-                  name="article_title"
-                  type="text"
-                  value={article_title}
-                  placeholder="Titre..."
-                  onChange={(e) => handleChange(e)}
-                  required
-                />
-                <span className="bar" />
-              </div>
-              <div className="input-group">
-                <i className="news-description" />
-                <input
-                  name="description"
-                  type="text"
-                  value={description}
-                  placeholder="Description..."
-                  onChange={(e) => handleChange(e)}
-                />
-                <span className="bar" />
-              </div>
-              <div className="input-group">
-                <i className="news-price" />
-                <input
-                  name="price"
-                  type="number"
-                  value={price}
-                  placeholder="Prix..."
-                  onChange={(e) => handleChange(e)}
-                />
-                <span className="bar" />
-              </div>
-              <div className="input-group">
-                <i className="picture-downlaod" />
-                <input
-                  name="picture_url"
-                  type="file"
-                  value={picture_url}
-                  accept="image"
-                  onChange={(e) => handleChange(e)}
-                />
-                <span className="bar" />
-              </div>
-              <div className="input-group">
+    <section className="section-addnews-form">
+      <div id="wraper">
+        <div className="form-container">
+          <span className="form-heading">Ajouter une News</span>
+          <form
+            method="post"
+            className="form-body"
+            onSubmit={handleSubmit}
+          >
+            <div className="input-group">
+              <i className="news-title" />
+              <input
+                name="article_title"
+                type="text"
+                value={article_title}
+                placeholder="Titre..."
+                onChange={(e) => handleChange(e)}
+                required
+              />
+              <span className="bar" />
+            </div>
+            <div className="input-group">
+              <i className="news-description" />
+              <input
+                name="description"
+                type="text"
+                value={description}
+                placeholder="Description..."
+                onChange={(e) => handleChange(e)}
+              />
+              <span className="bar" />
+            </div>
+            <div className="input-group">
+              <i className="news-price" />
+              <input
+                name="price"
+                type="number"
+                value={price}
+                placeholder="Prix..."
+                onChange={(e) => handleChange(e)}
+              />
+              <span className="bar" />
+            </div>
+            <div className="input-group">
+              <i className="picture-downlaod" />
+              <input
+                name="picture_url"
+                type="file"
+                value={picture_url}
+                accept="image"
+                onChange={(e) => handleChange(e)}
+              />
+              <span className="bar" />
+            </div>
+            <div className="input-group">
+              <button className="news-valid-form-but">
+                <i className="picture-valid">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-telegram" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+                  </svg>
+                </i>
+              </button>
+              <NavLink to={`/commercant/profil/${userId}`}>
                 <button className="news-valid-form-but">
                   <i className="picture-valid">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-telegram" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                      <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+                      <line x1="18" y1="6" x2="6" y2="18" />
+                      <line x1="6" y1="6" x2="18" y2="18" />
                     </svg>
                   </i>
                 </button>
-                <NavLink to={`/commercant/profil/${userId}`}>
-                  <button className="news-valid-form-but">
-                    <i className="picture-valid">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="18" y1="6" x2="6" y2="18" />
-                        <line x1="6" y1="6" x2="18" y2="18" />
-                      </svg>
-                    </i>
-                  </button>
-                </NavLink>
-              </div>
-            </form>
-          </div>
+              </NavLink>
+            </div>
+          </form>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
+
   );
 };
 
