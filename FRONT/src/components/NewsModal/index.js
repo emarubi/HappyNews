0fe59ce62733f49fdbda9c69b2832dc46b7cf7f3
thobenayann <thobena.yann@orange.fr>
2021-02-06 13:25:49 +0100

    style(main-integration):modal-background-fix

--- FRONT/src/components/NewsModal/index.js
+++ FRONT/src/components/NewsModal/index.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { MdDeleteForever } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
 import PopUp from 'src/containers/popup';
 import './style.scss';
 
@@ -11,38 +12,72 @@ const NewsModal = ({
   const changePopup = () => {
     setPopUp(!popUp);
   };
+
+  const [modalState, setModalState] = useState(false);
+  const manageState = () => {
+    setModalState(!modalState);
+  };
+
   return (
-    <section>
-      <div className="product-card">
-        <div className="badge">Hot  </div>
+    <>
+      <section>
+        <div className="product-card">
+          <div className="badge">News  </div>
 
-        <div className="product-tumb">
-          <img src={news.picture_url} alt="news-image" />
-        </div>
-        <div className="product-details">
-          <span className="product-catagory">{news.activity_name}</span>
-          <h4><a href="">{news.article_title}</a></h4>
-          <p>{news.description}</p>
-          <div className="product-bottom-details">
-            <div className="product-price"><small>$96.00</small>${news.price}</div>
-            <div className="product-links">
-              { parseInt(localStorage.getItem('id')) === news.user_id
-                && (
-                  <>
-                    <MdDeleteForever onClick={() => {
-                      changePopup(); console.log(popUp);
-                    }}
-                    />
-                    {popUp === true
-                    && <PopUp news={news} changePopup={changePopup} />}
-                  </>
+          <div className="product-tumb">
+            <img src={news.picture_url} alt="news" onClick={() => manageState(!modalState)} />
+          </div>
+          <div className="product-details">
+            <span className="product-catagory">{news.activity_name}</span>
+            <h4><a href="">{news.article_title}</a></h4>
+            <p>{news.description}</p>
+            <div className="product-bottom-details">
+              <div className="product-price"><small>$96.00</small>${news.price}</div>
+              <div className="product-links">
+                <NavLink to={`/commercant/profil/${news.user_id}`}>Voir le profil de ce commercant</NavLink>
+                { parseInt(localStorage.getItem('id'), 10) === news.user_id
+                  && (
+                    <>
+                      <MdDeleteForever onClick={() => {
+                        changePopup(); console.log(popUp);
+                      }}
+                      />
+                      {popUp === true
+                      && <PopUp news={news} changePopup={changePopup} />}
+                    </>
 
-                )}
+                  )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      <section>
+        <div className={`modalBackground modalShowing-${modalState} product-modal`}>
+          <div className="container-modal">
+            <div className="badge-modal">News</div>
+            <div className="product-tumb-modal">
+              <img src={news.picture_url} alt="news-modal" className="modal-picture" />
+            </div>
+            <div className="product-details-modal">
+              <span className="product-catagory-modal">{news.activity_name}</span>
+              <h4><a href="">{news.article_title}</a></h4>
+              <p>{news.description}</p>
+              <div className="product-bottom-details-modal">
+                <div className="product-price-modal"><small>$96.00</small>${news.price}</div>
+                <div className="product-links-modal">
+                  <button type="button" onClick={() => manageState(!modalState)}>
+                    fermer la modale
+                  </button>
+                  <NavLink to={`/commercant/profil/${news.user_id}`}>Voir le profil de ce commercant</NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
