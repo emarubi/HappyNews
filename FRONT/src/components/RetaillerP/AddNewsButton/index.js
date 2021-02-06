3543d723d0fd57769f0bbaa863ac68ca819cd94a
asmatar <deruelle.arthur@gmail.com>
2021-02-06 10:04:41 +0100

    nothing

--- FRONT/src/components/RetaillerP/AddNewsButton/index.js
+++ FRONT/src/components/RetaillerP/AddNewsButton/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './style.scss';
 
-const AddNewsButton = () => {
+const AddNewsButton = () =>
   // console.log(store);
-  return (
+  (
     <div className="bouton">
       <NavLink to="/creation-de-news">
         <button className="btn">Ajouter une News</button>
       </NavLink>
-      
+
     </div>
-  )
-}
-
-export default AddNewsButton
-
+  );
+export default AddNewsButton;
