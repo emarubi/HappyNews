3535c2bbbb54745a442bfb27b14834901c782f2b
BijouDEFRE <fredericcreuseveau@gmail.com>
2021-02-16 11:58:50 +0100

    create-import-BDD

--- BACK/import/import.js
+++ BACK/import/import.js
@@ -4,14 +4,15 @@
 require('dotenv').config();
 const { Client } = require('pg');
 
-const posts = require('../data/posts.json');
-const categories = require('../data/categories.json');
+const activities = require('../data/activities.json');
+const users = require('../data/users.json');
+const articles = require('../data/articles.json');
 
 // Instant Invoke function execution
 // Cela nous pemettra d'utiliser await directement dans notre fichier
 (async () => {
     const client = new Client({
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.PG_URL,
     });
     await client.connect();
 
@@ -19,37 +20,65 @@ const categories = require('../data/categories.json');
     // d'erreur sur des "UNIQUE" il vaut "vider" mes table avec de le faire tourner
     // TRUNCATE TABLE vide les tables données
     // et RESTART IDENTITY réinitialise les id à 1
-    await client.query('TRUNCATE TABLE post, category RESTART IDENTITY');
+    await client.query('TRUNCATE TABLE "acitivity", "user", "article" RESTART IDENTITY');
 
-    // Je vais stocker dans cet objet les id de mes catégories
-    const categoryIdMap = {};
-
-    for (let category of categories) {
-        // Avec le mot clé RETURNING je vais pouvoir récupérer l'id de la ligne enregistré
-        const result = await client.query(
-            'INSERT INTO category(label, route) VALUES ($1, $2) RETURNING id',
-            [category.label, category.route]);
-        // Je peux stocker cet id "en face" du label de category pour le retrouver plus facilement
-        categoryIdMap[category.label] = result.rows[0].id;
-        console.log(`Category "${category.label}" inserted`);
-    }
-
-    console.log('Category id map :', categoryIdMap);
-
-    for (let post of posts) {
+    for (let activity of activities) {
         await client.query(
-            `INSERT INTO post(title, slug, excerpt, content, category_id)
-                      VALUES ($1, $2, $3, $4, $5)`,
+            `INSERT INTO "activity"(activity_name)
+                        VALUES ($1)`,
             [
-                post.title,
-                post.slug,
-                post.excerpt,
-                post.content,
-                categoryIdMap[post.category]
+                activity.activity_name,
             ]);
 
-            console.log(`Post "${post.title}" inserted`);
+            console.log(`activity "${activity.activity_name}" inserted`);
     }
+
+    for (let user of users) {
+        await client.query(
+            `INSERT INTO "user"(id, first_name, last_name, email, password, adress, zip_code, city, latitude, longitude, shop_name, registration_number, role_id, content, activity_id)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)`,
+            [
+                user.id,
+                user.first_name,
+                user.last_name,
+                user.email,
+                user.password,
+                user.adress,
+                user.zip_code,
+                user.city,
+                user.latitude,
+                user.longitude,
+                user.shop_name,
+                user.registration_number,
+                user.role_id,
+                user.content,
+                user.activity_id,
+            ]);
+
+            console.log(`user "${user.id}" inserted`);
+    }
+
+    for (let article of articles) {
+        // Avec le mot clé RETURNING je vais pouvoir récupérer l'id de la ligne enregistré
+        const result = await client.query(
+            `INSERT INTO "article"(article_title, description, picture_url, price, is_news, news_duration, activity_id, user_id)
+                        VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`,
+            [
+                article.article_title,
+                article.description,
+                article.picture_url,
+                article.price,
+                article.is_news,
+                article.news_duration,
+                article.activity_id,
+                article.user_id,
+            ]);
+        // Je peux stocker cet id "en face" du user de article pour le retrouver plus facilement
+        articleIdMap[article.user] = result.rows[0].id;
+        console.log(`article "${article.user}" inserted`);
+    }
+
+    console.log('article id map :', articleIdMap);
 
     await client.end();
 })();
