bba891226a180406d8de7f9d44f694ede50be644
emarubi <69473685+emarubi@users.noreply.github.com>
2021-03-14 15:39:09 +0100

    chore(geolocation): add a button to geolocate on click and a search fiels on the Home Page map

--- FRONT/src/components/Home/Page/Map/index.js
+++ FRONT/src/components/Home/Page/Map/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -7,6 +8,9 @@ import {
   MapContainer, Marker, Popup, TileLayer, useMap,
 } from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/dist/geosearch.css';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+
 import placeholder from '../../../../assets/Images/placeholder.png';
 import './style.scss';
 
@@ -16,18 +20,60 @@ const visitorIcon = L.icon({
   iconSize: [40, 40],
 });
 
-const zoom = 14;
+const zoom = 5;
 const cityCoord = [48.864716, 2.349014];
 
 function Maps({
   cityCoordinates, getAllUsers, users,
 }) {
+  // define a state for an insrtance of map and for the position for geolocation
+  const [map, setMap] = useState(null);
+  const [position, setPosition] = useState();
+
+  // define a function to loacte on the map, using a button
+  function FlyToButton() {
+    const onClick = () => {
+      map.locate().on('locationfound', (e) => {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, 13);
+      });
+    };
+    // map.flyTo(regionCoord, zoom);
+    return <button type="button" className="locateButton" onClick={onClick}>Click pour visualiser les producteurs autour de chez vous</button>;
+  }
+
+  // define a function to geosearch on the map
+  function LeafletgeoSearch() {
+    // const map = useMap() // only if you dont have already an instance of map
+    useEffect(() => {
+      const provider = new OpenStreetMapProvider();
+
+      const searchControl = new GeoSearchControl({
+        style: 'bar',
+        position: 'topleft',
+        zoomLevel: 12,
+        provider,
+        marker: {
+          visitorIcon,
+        },
+      });
+
+      map.addControl(searchControl);
+
+      return () => map.removeControl(searchControl);
+      // eslint-disable-next-line
+    }, []);
+
+    return null;
+  }
+
   // define a state for an activeUser, when the visitor click on a user Marker on the map
   // if the vsitor doesn't select a user Marker, the activeUser is set at null in the state
   const [activeUser, setActiveUser] = useState(null);
 
   // const [users, setUsers] = useState([]);
 
+  // if coordinates come from search in NewsPage
   if (cityCoordinates.length) {
     // const cityCoordinatesToArray = cityCoordinates.split(',');
     const arrayofNumbers = cityCoordinates.map((element) => parseFloat(element));
@@ -42,61 +88,80 @@ function Maps({
   }, []);
 
   // const [map, setMap] = useState();
-  // visitor geoLocalisation on the Map
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
+  // visitor geoLocalisation on the Map when map renders (deprecated; now Localte on button)
+  // function LocationMarker() {
+  //   const [position, setPosition] = useState(null);
 
-    const map = useMap();
+  //   const map = useMap();
 
-    useEffect(() => {
-      map.locate().on('locationfound', (e) => {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-      });
-      // Stops watching location previously initiated by map.locate
-      return function cleanup() {
-        map.stopLocate();
-      };
-    }, []);
-    // the code above, if you want to geolocate on click
-    // const map = useMapEvents({
-    //   click() {
-    //     map.locate();
-    //   },
-    //   locationfound(e) {
-    //     setPosition(e.latlng);
-    //     map.flyTo(e.latlng, map.getZoom());
-    //   },
-    // });
+  //   useEffect(() => {
+  //     map.locate().on('locationfound', (e) => {
+  //       setPosition(e.latlng);
+  //       map.flyTo(e.latlng, map.getZoom());
+  //     });
+  //     // Stops watching location previously initiated by map.locate
+  //     return function cleanup() {
+  //       map.stopLocate();
+  //     };
+  //   }, []);
+  // // the code above, if you want to geolocate on click
+  // // const map = useMapEvents({
+  // //   click() {
+  // //     map.locate();
+  // //   },
+  // //   locationfound(e) {
+  // //     setPosition(e.latlng);
+  // //     map.flyTo(e.latlng, map.getZoom());
+  // //   },
+  // // });
 
-    return position === null ? null : (
-      <Marker
-        position={position}
-        // riseOnHover
-        icon={visitorIcon}
-      >
-        <Popup>Vous êtes ici</Popup>
-      </Marker>
-    );
-  }
+  //   return position === null ? null : (
+  //     <Marker
+  //       position={position}
+  //       // riseOnHover
+  //       icon={visitorIcon}
+  //     >
+  //       <Popup>Vous êtes ici</Popup>
+  //     </Marker>
+  //   );
+  // }
 
   return (
     <>
       <section className="map__container">
+        {/* create the button to geolocate */}
+        {map && <FlyToButton />}
+        <span className="spanText"> ou rentrez une adresse dans le champ ci-dessous</span>
+
+        {/* if ccordinates come from News page or rom props */}
         {cityCoord
       && (
+        /* create a map with coordinates from props */
         <MapContainer
           center={cityCoord}
           zoom={zoom}
-          scrollWheelZoom={false}
+          whenCreated={setMap}
+          // scrollWheelZoom={false}
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {!cityCoordinates.length
-          && <LocationMarker />}
-
+          {/* {!cityCoordinates.length
+          && <LocationMarker />} */}
+          {/* add geosearch if a map instance exists */}
+          {map && <LeafletgeoSearch />}
+          {/* add a placeholder on the default position defined by regionCoord */}
+          <Marker position={cityCoord} icon={visitorIcon}>
+            <Popup>Vous êtes ici</Popup>
+          </Marker>
+          {/* if position is located put a marker on new coordinates */}
+            {position && (
+              <Marker position={position} icon={visitorIcon}>
+                <Popup>Vous êtes ici</Popup>
+              </Marker>
+            )}
+          {/* users coordinates from axios request to user table */}
           {users.length
         && users.map((user) => (
           (!isNaN(parseFloat(user.latitude)) || !isNaN(parseFloat(user.longitude)))
